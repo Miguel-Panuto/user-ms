a6f32cb4fc919bf2eb5024710e3a2bbafb8d98ef
Miguel Panuto <miguel.panuto@live.com>
2021-10-16 16:21:48 -0300

    fix typo

--- src/app/user/entities/UpdateUserEntity.js
+++ src/app/user/entities/UpdateUserEntity.js
@@ -2,7 +2,7 @@ module.exports = (user) => {
   const obj = {
     name: user.name || null,
     document: user.document || null,
-    email: user.document || null,
+    email: user.email || null,
   };
   Object.keys(obj).forEach((u) => {
     if (obj[u] === null) delete obj[u];
